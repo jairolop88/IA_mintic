9e380e8c9758ccd1c705a69341672fc7dda6b18a
jairo_lopez <jairolop88@users.noreply.github.com>
2020-02-17 23:51:46 -0500

    Create test_turtle.py

---
+++ curso_python/test_turtle.py
@@ -0,0 +1,19 @@
+import turtle
+
+window = turtle.Screen()
+tortuga = turtle.Turtle()
+
+tortuga.forward(50)
+tortuga.right(60)
+tortuga.forward(50)
+tortuga.right(60)
+tortuga.forward(50)
+tortuga.right(60)
+tortuga.forward(50)
+tortuga.right(60)
+tortuga.forward(50)
+tortuga.right(60)
+tortuga.forward(50)
+tortuga.right(60)
+
+turtle.mainloop()
