e5d471ecf543335ad54a846d47a502b09920d39f
jairo_lopez <jairolop88@users.noreply.github.com>
2020-02-20 00:11:17 -0500

    se acomoda el codigo para funcionar en version 2 y 3 de python

--- curso_python/test_escritura.py
+++ curso_python/test_escritura.py
@@ -1,11 +1,19 @@
 # -*- coding: utf-8 -*-
+# para escribir caracteres con tildes, al string que se vaya a escribir se debe anteponer la letra "u"
 
 def run():
 
-    nombre = raw_input('escribe tu nombre: ')
-    edad = int(raw_input('escribe tu edad: '))
+# para la version 3 de python el metodo raw_input(), paso a ser solamente input()
 
-    print(u'me llamo ${} y tengo ${} años'.format(nombre,edad))
+    nombre = raw_input('escribe tu nombre: ') #para version python 2
+    edad = int(raw_input('escribe tu edad: ')) #para version python 2
+
+    #nombre = input('escribe tu nombre: ') #para version python 3
+    #edad = int(input('escribe tu edad: ')) #para version python 3
+
+    
+    print(u'me llamo {} y tengo {} años'.format(nombre,edad)) #para version python 2
+    #print('me llamo {} y tengo {} años'.format(nombre,edad)) #para version python 3
 
 
 if __name__ == '__main__':
