9ec7911b93faf23afc7eb47598b94ff78ad0818d
jairo_lopez <jairolop88@users.noreply.github.com>
2020-02-20 01:34:16 -0500

    curso python

--- curso_python/test_escritura.py
+++ curso_python/test_escritura.py
@@ -5,15 +5,15 @@ def run():
 
 # para la version 3 de python el metodo raw_input(), paso a ser solamente input()
 
-    nombre = raw_input('escribe tu nombre: ') #para version python 2
-    edad = int(raw_input('escribe tu edad: ')) #para version python 2
+    #nombre = raw_input('escribe tu nombre: ') #para version python 2
+    #edad = int(raw_input('escribe tu edad: ')) #para version python 2
 
-    #nombre = input('escribe tu nombre: ') #para version python 3
-    #edad = int(input('escribe tu edad: ')) #para version python 3
+    nombre = input('escribe tu nombre: ') #para version python 3
+    edad = int(input('escribe tu edad: ')) #para version python 3
 
     
-    print(u'me llamo {} y tengo {} años'.format(nombre,edad)) #para version python 2
-    #print('me llamo {} y tengo {} años'.format(nombre,edad)) #para version python 3
+    #print(u'me llamo {} y tengo {} años'.format(nombre,edad)) #para version python 2
+    print('me llamo {} y tengo {} años'.format(nombre,edad)) #para version python 3
 
 
 if __name__ == '__main__':
--- curso_python/test_function.py
+++ curso_python/test_function.py
@@ -1,1 +1,8 @@
 # -*- coding: utf-8 -*-    
+
+def run():
+   nombre = input('indica tu nombre')
+   print('hola {} como estas'.format(nombre))
+
+if __name__ == '__main__':
+   run()
